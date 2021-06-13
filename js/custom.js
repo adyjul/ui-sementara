c17a231ce8a4030aa4dd2b2d6feec36bf80b77ae
Ady Juliyanto <adyjuliyanto2001@gmail.com>
2021-06-13 13:50:49 +0700

    fix JS file

--- js/custom.js
+++ js/custom.js
@@ -1,113 +1,114 @@
-(function ($) {
-	
-	"use strict";
+// (function ($) {
+//   "use strict";
 
-	$('.scroll-to-section').on('click',function(e){
-		var href = $(this).attr('href');
-		
-		var elementHref = $(href);
-		
-		$('body').animate({
-			scrollTop : elementHref.offset().top - 50
-		},2000,'swing');
+$(document).ready(function () {
+  //   $(".scroll-to-section").on("click", function (e) {
+  //     var href = $(this).attr("href");
 
+  //     var elementHref = $(href);
 
-		//e.preventDefault();
-		
-	})
+  //     $("body").animate(
+  //       {
+  //         scrollTop: elementHref.offset().top - 50,
+  //       },
+  //       2000,
+  //       "swing"
+  //     );
 
+  //     //e.preventDefault();
+  //   });
 
+  var scrollLink = $(".scroll-to-section");
+  scrollLink.click(function (e) {
+    e.preventDefault();
+    $("body,html").animate(
+      {
+        scrollTop: $(this.hash).offset().top - 70,
+      },
+      1000,
+      "easeInOutExpo"
+    );
+  });
 
-	$(window).scroll(function() {
-	  var scroll = $(window).scrollTop();
-	  var box = $('.left-content').height()-20;
-	  var header = $('.navbar').height();
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var box = $(".left-content").height() - 20;
+    var header = $(".navbar").height();
 
-	  if (scroll >= box - header) {
-	    $(".navbar").addClass("fixed-top");
-	    $(".navbar").css("position","fixed");
-		
-	  } else {
-	    $(".navbar").removeClass("fixed-top");
- 	   $(".navbar").css("position","");
-	
-	  }
-	});
-	
-	$('.input-group.date').datepicker({format: "dd.mm.yyyy"});
-	
+    if (scroll >= box - header) {
+      $(".navbar").addClass("fixed-top");
+      $(".navbar").css("position", "fixed");
+    } else {
+      $(".navbar").removeClass("fixed-top");
+      $(".navbar").css("position", "");
+    }
+  });
 
-	$(".lomba-1").owlCarousel({
-		autoplay: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
-	});
-   
-	$(".guru-1").owlCarousel({
-		autoplay: true,
-        dots: false,
-        loop: true,
-        nav : true,
-        responsive: {
-            0:{
-                items:1
-            },
-            576:{
-                items:1
-            },
-            768:{
-                items:2
-            },
-            992:{
-                items:3
-            }
-        }
+  $(".input-group.date").datepicker({ format: "dd.mm.yyyy" });
 
-	});
+  $(".lomba-1").owlCarousel({
+    autoplay: true,
+    dots: false,
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 
+  $(".guru-1").owlCarousel({
+    autoplay: true,
+    dots: false,
+    loop: true,
+    nav: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 1,
+      },
+      768: {
+        items: 2,
+      },
+      992: {
+        items: 3,
+      },
+    },
+  });
 
+  // Window Resize Mobile Menu Fix
+  mobileNav();
 
+  // Scroll animation init
+  window.sr = new scrollReveal();
 
-	// Window Resize Mobile Menu Fix
-	mobileNav();
+  // Window Resize Mobile Menu Fix
+  $(window).on("resize", function () {
+    mobileNav();
+  });
 
+  // Window Resize Mobile Menu Fix
+  function mobileNav() {
+    var width = $(window).width();
+    $(".submenu").on("click", function () {
+      if (width < 767) {
+        $(".submenu ul").removeClass("active");
+        $(this).find("ul").toggleClass("active");
+      }
+    });
+  }
+});
 
-	// Scroll animation init
-	window.sr = new scrollReveal();
-	
-
-
-	// Window Resize Mobile Menu Fix
-	$(window).on('resize', function() {
-		mobileNav();
-	});
-
-
-	// Window Resize Mobile Menu Fix
-	function mobileNav() {
-		var width = $(window).width();
-		$('.submenu').on('click', function() {
-			if(width < 767) {
-				$('.submenu ul').removeClass('active');
-				$(this).find('ul').toggleClass('active');
-			}
-		});
-	}
-
-
-})(window.jQuery);   
+// })(window.jQuery);
